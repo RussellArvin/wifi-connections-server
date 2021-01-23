6d943660f2b4f537b91df0df1a448b85dbe66a96
RussellArvinSP <russell.18@ichat.sp.edu.sg>
2021-01-23 19:43:03 +0800

    Handling disconnections

--- index.js
+++ index.js
@@ -24,18 +24,25 @@ function main(){
         for(const device of devices){
             if(!locateMAC(device.mac)){//If mac address is not stored on local server memory
                 connections.push({mac:device.mac, date:date}) //Push to array of connections with connection DateTime and IP
-                connectionQuery(device.mac,date) //Sending Connection data to Dynamo
+                connectionQuery(device.mac,date,"Connected") //Sending Connection data to Dynamo
             }         
+        }
+        const toBeDeleted = locateDisconnected(devices); // array of indexes within the connection array that are no longer connected to the network
+        if(toBeDeleted.length != 0){
+            for(const i of toBeDeleted){
+                connectionQuery(connections[i].mac,date,"Disconnected"); // Sending Connection Data to Dynamo
+                connections.splice(i,1); //Removing from Connection Array
+            }
         }
       })
 }
 
 //Insert Connection to DynamoDB
-function connectionQuery(mac,date){
+function connectionQuery(mac,date,type){
     const obj = {
         id: uuid(),
         mac: mac,
-        info: `Connected at ${date.toString().substring(3,15)} ${formatTime(date)}`
+        info: `${type} at ${date.toString().substring(3,15)} ${formatTime(date)}`
     }
 
     const params = {
@@ -54,6 +61,22 @@ function locateMAC(mac){
         if(obj.mac == mac) return true;
     }
     return false;
+}
+
+//Locate disconnected mac addresses
+function locateDisconnected(devices){
+    var duplicates = [];
+    var toBeDeleted = [];
+    for(const [i,con] of connections.entries()){
+        for(const device of devices){
+            if(con.mac == device.mac) duplicates.push(i);
+        }
+    }
+
+    for(var i = connections.length-1; i != -1; i--){
+        if(!duplicates.includes(i)) toBeDeleted.push(i);
+    }
+    return toBeDeleted;
 }
 
 //Format Time for Display
